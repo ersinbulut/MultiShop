3f2d3f568a784dc3b7d035d419b1ac8e35540226
DEV <DEV@DESKTOP-8VP4IIT>
2024-07-17 17:36:08 +0300

    220. Admin Paneli Yorum Güncelleme Sayfası

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values=_context.UserComments.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateComment(UserComment comment)
+        {
+            _context.UserComments.Add(comment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            _context.UserComments.Remove(value);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var values = _context.UserComments.Find(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment comment)
+        {
+            _context.UserComments.Update(comment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi");
+        }
+    }
+}
