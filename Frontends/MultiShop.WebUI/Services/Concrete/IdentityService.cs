0f22bf3966e68ca6d7ab1ecc6e1c2f4040d61eba
DEV <DEV@DESKTOP-8VP4IIT>
2024-08-05 12:47:55 +0300

    255. Discovery End Point

---
+++ Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,42 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Settings;
+
+namespace MultiShop.WebUI.Services.Concrete
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+        }
+
+        public async Task<bool> SignIn(SignUpDto signUpDto)
+        {
+            var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = "http://localhost:5001",
+                Policy = new DiscoveryPolicy
+                {
+                    RequireHttps = false
+                }
+            });
+            var passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = _clientSettings.MultiShopManagerId.ClientId,
+                ClientSecret = _clientSettings.MultiShopManagerId.ClientSecret,
+                UserName = signUpDto.Username,
+                Password = signUpDto.Password,
+                Address = discoveryEndPoint.TokenEndpoint
+            };
+        }
+    }
+}
